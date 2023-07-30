e639e5f6dbf18eb626466921d49129fe40dc91e3
Aman Verma <aman@Amans-MacBook-Air.local>
2023-07-30 14:16:40 +0530

    update client env

--- client/src/utils/axiosClient.js
+++ client/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let baseUrl = "http://localhost:4000/api/v1";
-if (process.env.NODE_ENV === "production") {
+if (process.env.REACT_APP_NODE_ENV === "production") {
   baseUrl = process.env.REACT_APP_BASE_URL;
 }
 const axiosClient = axios.create({
